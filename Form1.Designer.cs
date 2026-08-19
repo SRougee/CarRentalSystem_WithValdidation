89ccb159acc886833f65be3581f4861c4a438713
SRougee <ronaldjennings84@gmail.com>
2026-08-19 20:19:51 +0200

    Add car rental form UI controls and button handlers
    Added labels, textboxes, date pickers, and a combo box for car types to Form1.Designer.cs. Wired up Submit, Clear, and Exit buttons with event handlers in Form1.cs. Submit shows a MessageBox with input data, Clear resets fields, and Exit closes the app. Included placeholders for validation logic.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.dateRented = new System.Windows.Forms.DateTimePicker();
+            this.dateReturned = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.coboxTypeOfCar = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(252, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Car Rental Sysytem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(24, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label3.Location = new System.Drawing.Point(24, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Date Rented";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label4.Location = new System.Drawing.Point(390, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date Returned";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(48, 122);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(187, 22);
+            this.txtCustomerName.TabIndex = 4;
+            // 
+            // dateRented
+            // 
+            this.dateRented.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateRented.Location = new System.Drawing.Point(48, 203);
+            this.dateRented.Name = "dateRented";
+            this.dateRented.Size = new System.Drawing.Size(186, 22);
+            this.dateRented.TabIndex = 5;
+            // 
+            // dateReturned
+            // 
+            this.dateReturned.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateReturned.Location = new System.Drawing.Point(417, 204);
+            this.dateReturned.Name = "dateReturned";
+            this.dateReturned.Size = new System.Drawing.Size(186, 22);
+            this.dateReturned.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label5.Location = new System.Drawing.Point(24, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Type Of Car";
+            // 
+            // coboxTypeOfCar
+            // 
+            this.coboxTypeOfCar.FormattingEnabled = true;
+            this.coboxTypeOfCar.Items.AddRange(new object[] {
+            "Honda",
+            "Toyota",
+            "Lada",
+            "Beetle",
+            "Golf",
+            "Fox"});
+            this.coboxTypeOfCar.Location = new System.Drawing.Point(51, 301);
+            this.coboxTypeOfCar.Name = "coboxTypeOfCar";
+            this.coboxTypeOfCar.Size = new System.Drawing.Size(183, 24);
+            this.coboxTypeOfCar.TabIndex = 8;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(120, 347);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(150, 47);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(276, 347);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(150, 47);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(432, 347);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 47);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(721, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.coboxTypeOfCar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateReturned);
+            this.Controls.Add(this.dateRented);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.DateTimePicker dateRented;
+        private System.Windows.Forms.DateTimePicker dateReturned;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox coboxTypeOfCar;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
